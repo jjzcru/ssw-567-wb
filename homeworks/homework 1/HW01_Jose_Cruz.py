8cd574ae61d3cbc72aac7095e79a536fe6643723
Jose J. Cruz <jjzcru@gmail.com>
2021-09-09 19:46:28 -0400

    Add homework 1

---
+++ homeworks/homework 1/HW01_Jose_Cruz.py
@@ -0,0 +1,78 @@
+"""HW01: Testing triangle classification
+
+   Author: Jose J. Cruz
+   CWID: 10467076
+"""
+
+from typing import List
+
+
+def is_equilateral(*arg: tuple[int]):
+    if len(arg) != 3:
+        raise Exception('Requires three sides')
+
+    total: int = 0
+    for side in arg:
+        if not isinstance(side, int):
+            raise TypeError('Side must be an int')
+        total += side
+
+    return (total / 3) == arg[0]
+
+
+def is_isosceles(*arg: tuple[int]):
+    if len(arg) != 3:
+        raise Exception('Requires three sides')
+
+    for side in arg:
+        if not isinstance(side, int):
+            raise TypeError('Side must be an int')
+
+    return arg[0] == arg[1] or arg[1] == arg[2] or arg[2] == arg[0]
+
+
+def is_right(*arg: tuple[int]):
+    if len(arg) != 3:
+        raise Exception('Requires three sides')
+
+    for side in arg:
+        if not isinstance(side, int):
+            raise TypeError('Side must be an int')
+
+    sides = sorted(arg)
+
+    return (pow(sides[0], 2) + pow(sides[1], 2)) == pow(sides[2], 2)
+
+
+def classify_triangle(*arg: tuple[int]) -> str:
+    if is_equilateral(arg[0], arg[1], arg[2]):
+        return 'equilateral'
+    elif is_isosceles(arg[0], arg[1], arg[2]):
+        return 'isosceles'
+    elif is_right(arg[0], arg[1], arg[2]):
+        return 'right'
+    else:
+        return 'scalene'
+
+
+def main():
+    """Main program function"""
+    sides: List[int] = []
+    while not len(sides) == 3:
+        user_input = input(f'Insert the length of the side {len(sides) + 1}: \n')
+        try:
+            side = int(user_input)
+        except ValueError:
+            print(f'The value {user_input} is not an integer, please try again')
+        except:
+            print("Something else went wrong, Please try again")
+        else:
+            sides.append(side)
+
+    triangle_type: str = classify_triangle(sides[0], sides[1], sides[2])
+
+    print(f'\n-----------------------\n\nIs a "{triangle_type}" triangle')
+
+
+if __name__ == "__main__":
+    main()
---
+++ homeworks/homework 1/HW01_Test_Jose_Cruz.py
@@ -0,0 +1,38 @@
+"""HW01: Testing triangle classification
+
+   Author: Jose J. Cruz
+   CWID: 10467076
+"""
+import unittest
+from HW01_Jose_Cruz import is_equilateral, is_isosceles, classify_triangle
+
+
+class TriangleTest(unittest.TestCase):
+    """Test suite for Triangles function"""
+
+    def test_is_equilateral(self) -> None:
+        """Test if the triangle es equilateral"""
+        self.assertEqual(is_equilateral(2, 2, 2), True)
+        self.assertEqual(is_equilateral(2, 2, 1), False)
+        self.assertRaises(TypeError, is_equilateral, 1, 'a', True)
+        self.assertRaises(Exception, is_equilateral, 1, 1)
+
+    def test_is_isosceles(self) -> None:
+        """Test if the triangle is isosceles"""
+        self.assertEqual(is_isosceles(2, 3, 1), False)
+        self.assertEqual(is_isosceles(2, 3, 2), True)
+        self.assertRaises(TypeError, is_isosceles, 1, 'a', True)
+        self.assertRaises(Exception, is_isosceles, 1, 1)
+
+    def test_classify_triangle(self) -> None:
+        """Test if can return the correct type of triangle"""
+        self.assertEqual(classify_triangle(1, 1, 1), 'equilateral')
+        self.assertEqual(classify_triangle(2, 3, 2), 'isosceles')
+        self.assertEqual(classify_triangle(3, 4, 5), 'right')
+        self.assertEqual(classify_triangle(1, 2, 3), 'scalene')
+        self.assertRaises(TypeError, classify_triangle, 1, 'a', True)
+        self.assertRaises(Exception, classify_triangle, 1, 1)
+
+
+if __name__ == '__main__':
+    unittest.main(exit=False, verbosity=2)
